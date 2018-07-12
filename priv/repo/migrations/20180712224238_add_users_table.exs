4a0ef0aab7d5fece1caa4184d26ee57d1e6d7e47
Dawid Warpas <dawid.warpas@gmail.com>
2018-07-13 00:50:35 +0200

    CV-2: Add migration for basic user personal data

---
+++ priv/repo/migrations/20180712224238_add_users_table.exs
@@ -0,0 +1,15 @@
+defmodule ResumeGenerator.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add(:uuid, :binary_id, primary_key: true)
+      add(:name, :string)
+      add(:email, :string)
+      add(:phone, :string)
+      add(:website, :string)
+
+      timestamps()
+    end
+  end
+end
