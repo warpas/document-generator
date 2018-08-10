a461c1930ef9a8bbd5bd2673956d5d9694dd292b
Dawid Warpas <dawid.warpas@gmail.com>
2018-08-10 23:09:32 +0200

    CV-2-1: Add multiple resumes in testing

--- test/resume_generator_web/controllers/resume_controller_test.exs
+++ test/resume_generator_web/controllers/resume_controller_test.exs
@@ -3,22 +3,22 @@ defmodule ResumeGeneratorWeb.ResumeControllerTest do
   use ResumeGenerator.DataCase
 
   setup do
-    {:ok, resume: insert(:resume)}
+    {:ok, resumes: insert_list(10, :resume)}
   end
 
   describe "resume index action" do
     @tag :web
-    test "should succeed", %{conn: conn} do
+    test "should succeed", %{conn: conn, resumes: resumes} do
       conn = get conn, "/resumes"
       assert html_response(conn, 200) =~ "There will be a list of resume documents here at some point. "
+      # TODO: assert the list is working in CV-2-2
     end
   end
 
-  # TODO: Add test for show action
   describe "resume show action" do
     @tag :web
-    test "should succeed with valid ID", %{conn: conn, resume: resume} do
-      conn = get conn, "/resumes/#{resume.uuid}"
+    test "should succeed with valid ID", %{conn: conn, resumes: resumes} do
+      conn = get conn, "/resumes/#{List.first(resumes).uuid}"
       assert html_response(conn, 200) =~ "The input forms below will start working at some point."
     end
   end
